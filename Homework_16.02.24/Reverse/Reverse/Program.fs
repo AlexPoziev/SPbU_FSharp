d309acb2c54de42b80c6315d2d25f80ee74f6bbe
Aleksey Poziev <112957370+AlexPoziev@users.noreply.github.com>
2024-02-16 23:57:18 +0300

    feat: add impl

---
+++ Homework_16.02.24/Reverse/Reverse/Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+
+let testList =  [1..5]
+
+let reverse list =
+    let rec helper ls acc =
+        match ls with
+        | h :: t -> helper t (h :: acc)
+        | [] -> acc
+    helper list []
+
+testList |> reverse |> List.iter Console.WriteLine
