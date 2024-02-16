1048fd4454ff31bb6bb54a43dc51f181e5a5a6a8
Aleksey Poziev <112957370+AlexPoziev@users.noreply.github.com>
2024-02-16 23:39:42 +0300

    feat: add impl

---
+++ Homework_16.02.24/Fibonacci/Fibonacci/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+
+let intInput = Console.ReadLine() |> int
+
+let fibonacci number =
+    match number with
+    | x when x = 0 || x = 1 -> x
+    | x when x < 0 -> -1
+    | _ ->
+        let rec calculate (first, second) i =
+            if i = (number - 1) then second
+            else calculate(second, first + second) (i + 1)
+        calculate (1, 1) 1
+    
+let result = intInput |> fibonacci
+if result = -1 then printf "Number is less than 0"
+else printf $"Fibonacci number: %d{result}"
