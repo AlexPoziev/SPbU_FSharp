ab7eeb5bc49e0199cd00292eb6e0bd9cc4784848
Aleksey Poziev <112957370+AlexPoziev@users.noreply.github.com>
2024-02-17 19:17:34 +0300

    wip

--- Homework_16.02.24/Fibonacci/Fibonacci/Fibonacci.fs
+++ Homework_16.02.24/Fibonacci/Fibonacci/Fibonacci.fs
@@ -1,12 +1,10 @@
 ﻿module Fibonacci
 
 let fibonacci number =
-    match number with
-    | x when x < 0 -> None
-    | _ ->
-            let rec helper (first, second) i =
-                match i with
-                | i when i = number -> Some(first)
-                | _ -> helper (second, first + second) (i + 1)
-            helper (0, 1) 0
-            
+    if number < 0 then None
+    else 
+        let rec helper (first, second) i =
+            match i with
+            | i when i = number -> Some(first)
+            | _ -> helper (second, first + second) (i + 1)
+        helper (0, 1) 0
