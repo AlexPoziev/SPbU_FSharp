5106fb844880494a774ad757d0f8b839be45f354
Aleksey Poziev <112957370+AlexPoziev@users.noreply.github.com>
2024-02-17 12:05:40 +0300

    style

--- Homework_16.02.24/Factorial/FactorialTests/FactorialTests.fs
+++ Homework_16.02.24/Factorial/FactorialTests/FactorialTests.fs
@@ -6,11 +6,11 @@ open Factorial
 [<TestCase(0, 1)>]
 [<TestCase(1, 1)>]
 [<TestCase(5, 120)>]
-let Factorial_WithMoreThanZeroArgument_ReturnsExpectedValue (number, expected) =
+let Factorial_WithMoreThanZeroArgument_ShouldReturnExpectedValue (number, expected) =
     Assert.AreEqual(expected, match factorial number with | Some(x) -> x | None -> -1)
 
 [<Test>]
-let Factorial_WithLessThanZeroArgument_ReturnsNone () =
+let Factorial_WithLessThanZeroArgument_ShouldReturnNone () =
     let argument = -2
     let expected = None
     Assert.AreEqual(expected, factorial argument)   
